9b71688fcb64d3009efdbf206b0a97010ddce0a7
Ane Caroline <anebastos@bemol.com.br>
2020-05-19 12:05:29 -0400

    style: add style to comments

---
+++ src/components/Comment/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  viewComment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  imgSend: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+});
+
+export default styles;
