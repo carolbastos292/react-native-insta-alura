4529ea531a9838849196d5768fd57f73d60ae521
Ane Caroline <anebastos@bemol.com.br>
2020-05-19 12:02:41 -0400

    feat: comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,14 +1,51 @@
 import React, {Fragment, useState} from 'react';
-import {Image, Text, TouchableOpacity, View, FlatList} from 'react-native';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+  TextInput,
+} from 'react-native';
 import styles from './styles';
 
 const Comment = ({comments}) => {
+  const [stateComments, setComment] = useState(comments);
+  const addComment = () => {
+    //console.warn(contentFieldInput);
+    contentInput.clear();
+    const newComment = {
+      date: Date.now(),
+      text: contentFieldInput,
+      userName: 'Ane',
+    };
+
+    setComment([...stateComments, newComment]);
+  };
+  let contentInput;
+  let contentFieldInput = '';
   return (
-    <FlatList
-      data={comments}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({item}) => <Text>{item.text}</Text>}
-    />
+    <Fragment>
+      <FlatList
+        data={stateComments}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
+      <View style={styles.viewComment}>
+        <TextInput
+          ref={textInput => contentInput = textInput}
+          onChangeText={(text) => (contentFieldInput = text)}
+          style={{flex: 1}}
+          placeholder={'Deixe seu comentário...'}
+        />
+        <TouchableOpacity onPress={addComment}>
+          <Image
+            style={styles.imgSend}
+            source={require('../../res/img/send.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </Fragment>
   );
 };
 
